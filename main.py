05ec0a0c65f8d462e93580edff1262d9a6d2c028
cassina <bernardocassina@gmail.com>
2019-02-25 17:12:38 -0500

    II.10 Templates.

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def index():
     user_ip = request.remote_addr
 
-    return 'Hello World Platzi, tu IP es {}'.format(user_ip)
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+
+    return render_template('hello.html', user_ip=user_ip)
 
