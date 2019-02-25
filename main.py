5cb1d68f6e42052e01f869265b9254a255461ca5
cassina <bernardocassina@gmail.com>
2019-02-25 17:30:35 -0500

    II. 10. Template.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
+todos = ['TODO 1', 'TODO 2', 'TODO 3']
+
 
 @app.route('/')
 def index():
@@ -16,6 +18,10 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip': user_ip,
+        'todos': todos,
+    }
 
-    return render_template('hello.html', user_ip=user_ip)
+    return render_template('hello.html', **context)
 
