4d5a6f1adafc995bcdf795a5e5b4c4006697f40a
cassina <bernardocassina@gmail.com>
2019-02-25 21:47:43 -0500

    II.10 Links.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
-todos = ['TODO 1', 'TODO 2', 'TODO 3']
+todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
 
 
 @app.route('/')
