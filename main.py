bb88409bd3911251f72f485815ee3fffbed81bad
cassina <bernardocassina@gmail.com>
2019-02-26 14:59:34 -0500

    Clase 10: archivos estaticos  y Clase 11: error handlers.

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
