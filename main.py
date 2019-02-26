b03552132abc0d1ea23a0522ed96d18f39c2f49d
cassina <bernardocassina@gmail.com>
2019-02-26 15:38:58 -0500

    Clase 13 app config.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
+
 
 todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
 
@@ -22,14 +25,14 @@ def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     context = {
         'user_ip': user_ip,
         'todos': todos,
