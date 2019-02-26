d4e6d2058a30124bf32e07537e53d569e54d34fe
cassina <bernardocassina@gmail.com>
2019-02-26 15:56:02 -0500

    Clase 14 formas 1.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -8,6 +11,12 @@ app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 
 todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
 
 
 @app.errorhandler(404)
@@ -33,9 +42,12 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = session.get('user_ip')
+    login_form = LoginForm()
+
     context = {
         'user_ip': user_ip,
         'todos': todos,
+        'login_form': login_form
     }
 
     return render_template('hello.html', **context)
