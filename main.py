498e88b24cb15e38abe6cbee98ec8da1c4719216
cassina <bernardocassina@gmail.com>
2019-02-26 15:05:50 -0500

    Clase 11 Respuesta al reto error 500.

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ def not_found(error):
     return render_template('404.html', error=error)
 
 
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html', error=error)
+
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
