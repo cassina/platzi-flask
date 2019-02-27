f2a737bdd1baff4655527766c5e3bf56ba314cee
cassina <bernardocassina@gmail.com>
2019-02-27 16:34:21 -0500

    Clase 26 logout.

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,11 @@
 from flask import render_template, session, redirect, flash, url_for
+from flask_login import login_user, login_required, logout_user
 
 from app.forms import LoginForm
 
 from . import auth
+from app.firestore_service import get_user
+from app.models import UserModel, UserData
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -14,10 +17,36 @@ def login():
 
     if login_form.validate_on_submit():
         username = login_form.username.data
-        session['username'] = username
+        password = login_form.password.data
 
-        flash('Nombre de usario registrado con éxito!')
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is not None:
+            password_from_db = user_doc.to_dict()['password']
+
+            if password == password_from_db:
+                user_data = UserData(username, password)
+                user = UserModel(user_data)
+
+                login_user(user)
+
+                flash('Bienvenido de nuevo')
+
+                redirect(url_for('hello'))
+            else:
+                flash('La informaición no coincide')
+        else:
+            flash('El usario no existe')
 
         return redirect(url_for('index'))
 
     return render_template('login.html', **context)
+
+
+@auth.route('logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Regresa pronto')
+
+    return redirect(url_for('auth.login'))
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import unittest
 from flask import request, make_response, redirect, render_template, session, url_for, flash
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 
 from app import create_app
@@ -40,7 +40,7 @@ def index():
 @login_required
 def hello():
     user_ip = session.get('user_ip')
-    username = session.get('username')
+    username = current_user.id
 
     context = {
         'user_ip': user_ip,
